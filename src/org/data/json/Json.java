9ef6325aae63ad30323755288874ac15704b06da
Subhajit Sahu <wolfram77@gmail.com>
2015-04-13 17:52:03 +0530

    added toString method

--- src/org/data/json/Json.java
+++ src/org/data/json/Json.java
@@ -1,8 +1,24 @@
 // @wolfram77
 package org.data.json;
 
+// required modules
+import java.util.*;
 
 
-public class Json {
+
+// suggest json -> jap / jmap
+// suggest jsonlist -> jist / jlist
+public class Json extends HashMap<String, Object> {
     
+    @Override
+    public String toString() {
+        StringBuilder ans = new StringBuilder("{");
+        for(Map.Entry<String, Object> o : entrySet()) {
+            ans.append('\"').append(o.getKey()).append("\":");
+            ans.append(o.getValue()).append(",");
+        }
+        if(ans.length() > 1) ans.deleteCharAt(ans.length()-1);
+        ans.append("}");
+        return ans.toString();
+    }
 }
