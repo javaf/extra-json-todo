010557a9ac42bf4ecf5a81fc54231714d4442c05
Subhajit Sahu <wolfram77@gmail.com>
2015-04-16 09:48:32 +0530

    Jist ready to start

--- src/org/data/json/Jist.java
+++ src/org/data/json/Jist.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// @wolfram77
 package org.data.json;
 
-/**
- *
- * @author verizon
- */
+
+
 public class Jist {
     
 }
