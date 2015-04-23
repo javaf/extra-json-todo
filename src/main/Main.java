4a55037d728530b8ad60ca0ec6beeaa516410b36
Subhajit Sahu <wolfram77@gmail.com>
2015-04-23 08:45:06 +0530

    ParseToken test ok

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ import org.data.json.*;
 public class Main {
     
     public static void main(String[] args) {
-        Text text = new Text("\\n", 0, 2);
+        Text text = new Text("\\uabcd", 0, 6);
         System.out.println("text = "+text);
         Parser parser = new Parser();
         ParseToken token = parser.escSeq(text);
